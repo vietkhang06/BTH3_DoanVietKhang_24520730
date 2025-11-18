dfb92abf19416be4345c194c0369244d4ab35f5b
vietkhang <109060462+vietkhang06@users.noreply.github.com>
2025-11-18 20:09:17 +0700

    Add files via upload

---
+++ BTH3_Bai5/BTH3_Bai5/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace BTH3_Bai5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDivine = new System.Windows.Forms.Button();
+            this.tbAnswer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(193, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(193, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number 2";
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Location = new System.Drawing.Point(293, 70);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(200, 26);
+            this.tbNum1.TabIndex = 2;
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Location = new System.Drawing.Point(293, 151);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(200, 26);
+            this.tbNum2.TabIndex = 3;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(200, 225);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(50, 43);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(278, 225);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(50, 43);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(355, 225);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(50, 43);
+            this.btnMulti.TabIndex = 6;
+            this.btnMulti.Text = "X";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnDivine
+            // 
+            this.btnDivine.Location = new System.Drawing.Point(433, 225);
+            this.btnDivine.Name = "btnDivine";
+            this.btnDivine.Size = new System.Drawing.Size(50, 43);
+            this.btnDivine.TabIndex = 7;
+            this.btnDivine.Text = "/";
+            this.btnDivine.UseVisualStyleBackColor = true;
+            this.btnDivine.Click += new System.EventHandler(this.btnDivine_Click);
+            // 
+            // tbAnswer
+            // 
+            this.tbAnswer.Location = new System.Drawing.Point(293, 325);
+            this.tbAnswer.Name = "tbAnswer";
+            this.tbAnswer.Size = new System.Drawing.Size(200, 26);
+            this.tbAnswer.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(193, 328);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Answer";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbAnswer);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnDivine);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.tbNum2);
+            this.Controls.Add(this.tbNum1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDivine;
+        private System.Windows.Forms.TextBox tbAnswer;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BTH3_Bai5/BTH3_Bai5/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTH3_Bai5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private bool LayDuLieu(out double so1, out double so2)
+        {
+            so1 = 0;
+            so2 = 0;
+            bool check1 = double.TryParse(tbNum1.Text, out so1);
+            bool check2 = double.TryParse(tbNum2.Text, out so2);
+            if (!check1 || !check2)
+            {
+                MessageBox.Show("Vui lòng nhập số hợp lệ vào cả hai ô!", "Lỗi Đầu Vào", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            if (LayDuLieu(out double so1, out double so2))
+            {
+                double ketQua = so1 + so2;
+                tbAnswer.Text = ketQua.ToString();
+            }
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            if (LayDuLieu(out double so1, out double so2))
+            {
+                double ketQua = so1 - so2;
+                tbAnswer.Text = ketQua.ToString();
+            }
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+            if (LayDuLieu(out double so1, out double so2))
+            {
+                double ketQua = so1 * so2;
+                tbAnswer.Text = ketQua.ToString();
+            }
+        }
+
+        private void btnDivine_Click(object sender, EventArgs e)
+        {
+            if (LayDuLieu(out double so1, out double so2))
+            {
+                if (so2 == 0)
+                {
+                    MessageBox.Show("Không thể chia cho 0!", "Lỗi Phép Tính", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    tbAnswer.Text = "Không xác định";
+                }
+                else
+                {
+                    double ketQua = so1 / so2;
+                    tbAnswer.Text = ketQua.ToString();
+                }
+            }
+        }
+    }
+}
