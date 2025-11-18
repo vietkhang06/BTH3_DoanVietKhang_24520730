d8eb6e527adb520d74b94534cfc697975149415f
vietkhang <109060462+vietkhang06@users.noreply.github.com>
2025-11-18 20:14:21 +0700

    Add files via upload

---
+++ BTH3_Bai7/BTH3_Bai7/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace BTH3_Bai7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn10 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn15 = new System.Windows.Forms.Button();
+            this.btn14 = new System.Windows.Forms.Button();
+            this.btn13 = new System.Windows.Forms.Button();
+            this.btn12 = new System.Windows.Forms.Button();
+            this.btn11 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbCash = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnHuyBo = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.Color.Coral;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(635, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(204, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(212, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "MÀN ẢNH";
+            // 
+            // btn1
+            // 
+            this.btn1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn1.Location = new System.Drawing.Point(70, 142);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(80, 80);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = false;
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn2.Location = new System.Drawing.Point(170, 142);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(80, 80);
+            this.btn2.TabIndex = 2;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = false;
+            // 
+            // btn3
+            // 
+            this.btn3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn3.Location = new System.Drawing.Point(270, 142);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(80, 80);
+            this.btn3.TabIndex = 3;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = false;
+            // 
+            // btn4
+            // 
+            this.btn4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn4.Location = new System.Drawing.Point(370, 142);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(80, 80);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = false;
+            // 
+            // btn5
+            // 
+            this.btn5.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn5.Location = new System.Drawing.Point(470, 142);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(80, 80);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = false;
+            // 
+            // btn10
+            // 
+            this.btn10.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn10.Location = new System.Drawing.Point(470, 242);
+            this.btn10.Name = "btn10";
+            this.btn10.Size = new System.Drawing.Size(80, 80);
+            this.btn10.TabIndex = 10;
+            this.btn10.Text = "10";
+            this.btn10.UseVisualStyleBackColor = false;
+            // 
+            // btn9
+            // 
+            this.btn9.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn9.Location = new System.Drawing.Point(370, 242);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(80, 80);
+            this.btn9.TabIndex = 9;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = false;
+            // 
+            // btn8
+            // 
+            this.btn8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn8.Location = new System.Drawing.Point(270, 242);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(80, 80);
+            this.btn8.TabIndex = 8;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = false;
+            // 
+            // btn7
+            // 
+            this.btn7.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn7.Location = new System.Drawing.Point(170, 242);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(80, 80);
+            this.btn7.TabIndex = 7;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = false;
+            // 
+            // btn6
+            // 
+            this.btn6.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn6.Location = new System.Drawing.Point(70, 242);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(80, 80);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = false;
+            // 
+            // btn15
+            // 
+            this.btn15.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn15.Location = new System.Drawing.Point(470, 342);
+            this.btn15.Name = "btn15";
+            this.btn15.Size = new System.Drawing.Size(80, 80);
+            this.btn15.TabIndex = 15;
+            this.btn15.Text = "15";
+            this.btn15.UseVisualStyleBackColor = false;
+            // 
+            // btn14
+            // 
+            this.btn14.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn14.Location = new System.Drawing.Point(370, 342);
+            this.btn14.Name = "btn14";
+            this.btn14.Size = new System.Drawing.Size(80, 80);
+            this.btn14.TabIndex = 14;
+            this.btn14.Text = "14";
+            this.btn14.UseVisualStyleBackColor = false;
+            // 
+            // btn13
+            // 
+            this.btn13.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn13.Location = new System.Drawing.Point(270, 342);
+            this.btn13.Name = "btn13";
+            this.btn13.Size = new System.Drawing.Size(80, 80);
+            this.btn13.TabIndex = 13;
+            this.btn13.Text = "13";
+            this.btn13.UseVisualStyleBackColor = false;
+            // 
+            // btn12
+            // 
+            this.btn12.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn12.Location = new System.Drawing.Point(170, 342);
+            this.btn12.Name = "btn12";
+            this.btn12.Size = new System.Drawing.Size(80, 80);
+            this.btn12.TabIndex = 12;
+            this.btn12.Text = "12";
+            this.btn12.UseVisualStyleBackColor = false;
+            // 
+            // btn11
+            // 
+            this.btn11.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn11.Location = new System.Drawing.Point(70, 342);
+            this.btn11.Name = "btn11";
+            this.btn11.Size = new System.Drawing.Size(80, 80);
+            this.btn11.TabIndex = 11;
+            this.btn11.Text = "11";
+            this.btn11.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(149, 469);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "Thành tiền:";
+            // 
+            // tbCash
+            // 
+            this.tbCash.Location = new System.Drawing.Point(243, 466);
+            this.tbCash.Name = "tbCash";
+            this.tbCash.Size = new System.Drawing.Size(223, 26);
+            this.tbCash.TabIndex = 17;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(170, 531);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(90, 33);
+            this.btnSelect.TabIndex = 18;
+            this.btnSelect.Text = "Chọn";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnHuyBo
+            // 
+            this.btnHuyBo.Location = new System.Drawing.Point(264, 531);
+            this.btnHuyBo.Name = "btnHuyBo";
+            this.btnHuyBo.Size = new System.Drawing.Size(92, 33);
+            this.btnHuyBo.TabIndex = 19;
+            this.btnHuyBo.Text = "Hủy bỏ";
+            this.btnHuyBo.UseVisualStyleBackColor = true;
+            this.btnHuyBo.Click += new System.EventHandler(this.btnHuyBo_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(360, 531);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(90, 33);
+            this.btnEnd.TabIndex = 20;
+            this.btnEnd.Text = "Kết thúc";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.btnEnd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 576);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnHuyBo);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.tbCash);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn15);
+            this.Controls.Add(this.btn14);
+            this.Controls.Add(this.btn13);
+            this.Controls.Add(this.btn12);
+            this.Controls.Add(this.btn11);
+            this.Controls.Add(this.btn10);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "BÁN VÉ RẠP CHIẾU BÓNG";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn10;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn15;
+        private System.Windows.Forms.Button btn14;
+        private System.Windows.Forms.Button btn13;
+        private System.Windows.Forms.Button btn12;
+        private System.Windows.Forms.Button btn11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbCash;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnHuyBo;
+        private System.Windows.Forms.Button btnEnd;
+    }
+}
+
---
+++ BTH3_Bai7/BTH3_Bai7/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BTH3_Bai7
+{
+    public partial class Form1 : Form
+    {
+        private readonly int[] seatPrices = new int[16] { 0, 5000, 5000, 5000, 5000, 5000, 6500, 6500, 6500, 6500, 6500, 8000, 8000, 8000, 8000, 8000 };
+        private int[] seatStatus = new int[16];
+        private Button[] seatButtons;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupSeatControls();
+            SetupInitialState();
+            CalculateTotalPrice();
+        }
+
+        private void SetupSeatControls()
+        {
+            seatButtons = new Button[16];
+            for (int i = 1; i <= 15; i++)
+            {
+                seatButtons[i] = (Button)this.Controls.Find($"btn{i}", true)[0];
+                seatButtons[i].Click += SeatButton_Click;
+            }
+        }
+
+        private void SetupInitialState()
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                UpdateSeatColor(i);
+            }
+        }
+
+        private void UpdateSeatColor(int seatIndex)
+        {
+            switch (seatStatus[seatIndex])
+            {
+                case 0:
+                    seatButtons[seatIndex].BackColor = Color.White;
+                    seatButtons[seatIndex].ForeColor = Color.Black;
+                    break;
+                case 1:
+                    seatButtons[seatIndex].BackColor = Color.Yellow;
+                    seatButtons[seatIndex].ForeColor = Color.Black;
+                    break;
+                case 2:
+                    seatButtons[seatIndex].BackColor = Color.Blue;
+                    seatButtons[seatIndex].ForeColor = Color.White;
+                    break;
+            }
+        }
+
+        private void CalculateTotalPrice()
+        {
+            long totalPrice = 0;
+            for (int i = 1; i <= 15; i++)
+            {
+                if (seatStatus[i] == 2)
+                {
+                    totalPrice += seatPrices[i];
+                }
+            }
+            tbCash.Text = totalPrice.ToString("N0") + " VND";
+        }
+
+        private void SeatButton_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int seatIndex = int.Parse(clickedButton.Text);
+
+            if (seatStatus[seatIndex] == 1)
+            {
+                MessageBox.Show("Vị trí này đã được bán.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (seatStatus[seatIndex] == 0)
+            {
+                seatStatus[seatIndex] = 2;
+            }
+            else if (seatStatus[seatIndex] == 2)
+            {
+                seatStatus[seatIndex] = 0;
+            }
+
+            UpdateSeatColor(seatIndex);
+            CalculateTotalPrice();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            long totalMoney = 0;
+            long moneyToConfirm = 0;
+
+            for (int i = 1; i <= 15; i++)
+            {
+                if (seatStatus[i] == 2)
+                {
+                    moneyToConfirm += seatPrices[i];
+                }
+            }
+
+            if (moneyToConfirm > 0)
+            {
+                for (int i = 1; i <= 15; i++)
+                {
+                    if (seatStatus[i] == 2)
+                    {
+                        seatStatus[i] = 1;
+                        totalMoney += seatPrices[i];
+                        UpdateSeatColor(i);
+                    }
+                }
+
+                tbCash.Text = totalMoney.ToString("N0") + " VND";
+                MessageBox.Show($"Đã xác nhận mua vé. Tổng tiền: {totalMoney.ToString("N0")} VND.", "Thanh toán thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn ghế trước khi nhấn CHỌN.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                tbCash.Text = "0 VND";
+            }
+        }
+
+        private void btnHuyBo_Click(object sender, EventArgs e)
+        {
+            for (int i = 1; i <= 15; i++)
+            {
+                if (seatStatus[i] == 2)
+                {
+                    seatStatus[i] = 0;
+                    UpdateSeatColor(i);
+                }
+            }
+            tbCash.Text = "0 VND";
+        }
+
+        private void btnEnd_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
