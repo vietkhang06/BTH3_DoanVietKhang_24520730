a993c55eae6e92a0e225c8c084e79927b9a2d199
vietkhang <109060462+vietkhang06@users.noreply.github.com>
2025-11-18 20:03:08 +0700

    Add files via upload

---
+++ BTH3_Bai3/BTH3_Bai3/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BTH3_Bai3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnChangeColor = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnChangeColor
+            // 
+            this.btnChangeColor.BackColor = System.Drawing.Color.Gainsboro;
+            this.btnChangeColor.Location = new System.Drawing.Point(325, 190);
+            this.btnChangeColor.Name = "btnChangeColor";
+            this.btnChangeColor.Size = new System.Drawing.Size(141, 48);
+            this.btnChangeColor.TabIndex = 0;
+            this.btnChangeColor.Text = "Change Color";
+            this.btnChangeColor.UseVisualStyleBackColor = false;
+            this.btnChangeColor.Click += new System.EventHandler(this.btnChangeColor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnChangeColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnChangeColor;
+    }
+}
+
---
+++ BTH3_Bai3/BTH3_Bai3/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace BTH3_Bai3
+{
+    public partial class Form1 : Form
+    {
+        private Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeColor_Click(object sender, EventArgs e)
+        {
+            int r = rand.Next(256); 
+            int gr = rand.Next(256);
+            int b = rand.Next(256);
+            Color randomColor = Color.FromArgb(r, gr, b);
+            this.BackColor = randomColor;
+        }
+    }
+}
