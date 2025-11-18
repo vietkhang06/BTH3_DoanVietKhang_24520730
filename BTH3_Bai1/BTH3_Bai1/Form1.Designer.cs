0d33fc62dd9ddb5acf410702b5fbd544c9b561a0
vietkhang <109060462+vietkhang06@users.noreply.github.com>
2025-11-18 20:00:40 +0700

    Add files via upload

---
+++ BTH3_Bai1/BTH3_Bai1/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace BTH3_Bai1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lboxResult = new System.Windows.Forms.ListBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lboxResult
+            // 
+            this.lboxResult.BackColor = System.Drawing.Color.Honeydew;
+            this.lboxResult.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lboxResult.FormattingEnabled = true;
+            this.lboxResult.ItemHeight = 20;
+            this.lboxResult.Location = new System.Drawing.Point(0, 0);
+            this.lboxResult.Name = "lboxResult";
+            this.lboxResult.Size = new System.Drawing.Size(681, 585);
+            this.lboxResult.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnOpen.FlatAppearance.BorderSize = 0;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.ForeColor = System.Drawing.Color.White;
+            this.btnOpen.Location = new System.Drawing.Point(740, 48);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(236, 42);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open Model";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(687, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Thao Tác:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.CadetBlue;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(740, 96);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(236, 42);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "Close Form";
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Azure;
+            this.ClientSize = new System.Drawing.Size(1031, 585);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.lboxResult);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Move += new System.EventHandler(this.Form1_Move);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lboxResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+    }
+}
+
---
+++ BTH3_Bai1/BTH3_Bai1/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace BTH3_Bai1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LogEvent(string eventName)
+        {
+            string logEntry = $"[{DateTime.Now:HH:mm:ss.fff}] - Sự kiện: {eventName}{Environment.NewLine}";
+            lboxResult.Items.Add(logEntry);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LogEvent("Load Form, đang tải dữ liệu.");
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            LogEvent("Shown: Form đã hiển thị lần đầu.");
+        }
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            LogEvent("Activate: Form được kích hoạt.");
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            LogEvent("Deactivate: Form mất");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            LogEvent("Form closing: Form đang đóng");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            LogEvent("Form closed: Form đã đóng");
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Minimized)
+            {
+                LogEvent($"Resize: Kích thước mới ({this.Width}x{this.Height}).");
+            }
+        }
+
+        private void Form1_Move(object sender, EventArgs e)
+        {
+            LogEvent($"Move: Vị trí mới ({this.Location.X}, {this.Location.Y}).");
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            LogEvent("Click: 'Mở Modal Form'");
+
+            using (Form modalForm = new Form())
+            {
+                modalForm.Text = "Child Modal Form";
+                modalForm.Size = new Size(300, 150);
+                modalForm.StartPosition = FormStartPosition.CenterParent;
+                modalForm.ShowInTaskbar = false;
+                Label lbl = new Label
+                {
+                    Text = "Đóng form này để quay lại Form chính.",
+                    Dock = DockStyle.Fill,
+                    TextAlign = ContentAlignment.MiddleCenter
+                };
+                modalForm.Controls.Add(lbl);
+                LogEvent(">>> Chuẩn bị gọi ShowDialog()...");
+                modalForm.ShowDialog(this);
+                LogEvent(">>> ... ShowDialog() đã trả về.");
+            }
+        }
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Bạn có chắc muốn thoát?", "Xác nhận", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
