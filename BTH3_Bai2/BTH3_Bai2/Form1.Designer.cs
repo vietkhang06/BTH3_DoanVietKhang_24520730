50f77b47db3066292f925b2d1c73f4b816a78d99
vietkhang <109060462+vietkhang06@users.noreply.github.com>
2025-11-18 20:01:51 +0700

    Add files via upload

---
+++ BTH3_Bai2/BTH3_Bai2/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace BTH3_Bai2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRepaint = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRepaint
+            // 
+            this.btnRepaint.Location = new System.Drawing.Point(314, 55);
+            this.btnRepaint.Name = "btnRepaint";
+            this.btnRepaint.Size = new System.Drawing.Size(140, 44);
+            this.btnRepaint.TabIndex = 0;
+            this.btnRepaint.Text = "Repaint";
+            this.btnRepaint.UseVisualStyleBackColor = true;
+            this.btnRepaint.Click += new System.EventHandler(this.btnRepaint_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRepaint);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRepaint;
+    }
+}
+
---
+++ BTH3_Bai2/BTH3_Bai2/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace BTH3_Bai2
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            int x = rand.Next(0, this.ClientSize.Width - 100);
+            int y = rand.Next(0, this.ClientSize.Height - 30);
+            int r = rand.Next(256); 
+            int gr = rand.Next(256); 
+            int b = rand.Next(256);
+            Color randomColor = Color.FromArgb(r, gr, b);
+            string textToDraw = "Paint Event";
+            using (Font drawFont = new Font("Arial", 16, FontStyle.Bold))
+            using (Brush drawBrush = new SolidBrush(randomColor))
+            {
+                g.DrawString(textToDraw, drawFont, drawBrush, x, y);
+            }
+        }
+
+        private void btnRepaint_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+    }
+}
